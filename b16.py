b094fe83175b4836b8f8ae4b70406501ac3f638c
NguyenXuanAnhVy <anhvy12052005@gmail.com>
2024-08-28 19:36:21 +0700

    Add files via upload

---
+++ b16.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Aug 28 18:52:09 2024
+
+@author: Student
+"""
+
+import math
+h= int(input("Nhap gio:"))
+p= int(input("Nhap phut:"))
+s= int(input("Nhap giay:"))
+print(f"{h}h{p}p{s}")
+tonggiay= h*3600 + p*60 +s
+print("Tong giay: ", tonggiay)
