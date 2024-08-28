6c1e22127efe4e0ffc9f001a7a6b776bd555266a
NguyenXuanAnhVy <anhvy12052005@gmail.com>
2024-08-28 20:11:23 +0700

    Add files via upload

---
+++ b10.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Aug 28 19:41:57 2024
+
+@author: Student
+"""
+
+import math
+soxe= input("Nhap so xe cua ban (gom 4 chu so):")
+tong= int(soxe[0]) + int(soxe[1]) + int(soxe[2]) + int(soxe[3]) 
+sonut= tong%10
+print("So nut: ", sonut)
