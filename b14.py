4347bcd6d22f918b51b9d685d6cee97323af11e9
NguyenXuanAnhVy <anhvy12052005@gmail.com>
2024-08-28 18:43:35 +0700

    Add files via upload

---
+++ b14.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Aug 28 18:34:04 2024
+
+@author: Student
+"""
+
+import math
+A= (((32)**0.2)-((1/64)**(-0.25))+((8/27)**1/3))
+ket_qua= round(A, 3)
+print("Ket qua la= ", ket_qua)
