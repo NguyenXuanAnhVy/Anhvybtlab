d022ea0358798650444e6fa4f81c9cf33bbf44b6
NguyenXuanAnhVy <anhvy12052005@gmail.com>
2024-08-28 18:18:23 +0700

    Add files via upload

---
+++ b3.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Aug 28 18:16:00 2024
+
+@author: vy
+"""
+import math
+a= float(input("Nhap gia tri a= "))
+b= float(input("Nhap gia tri b= "))
+print("a%b=", a%b)
+print("a//b", a//b)
