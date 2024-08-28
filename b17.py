c99b1b49e416ce69d7761ed4de621d7fb3ad8db1
NguyenXuanAnhVy <anhvy12052005@gmail.com>
2024-08-28 20:03:39 +0700

    Add files via upload

---
+++ b17.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Aug 28 19:01:26 2024
+
+@author: Student
+"""
+import math
+a = int(input("Nhap so nguyen thu nhat: "))
+b = int(input("Nhap so nguyen thu hai: "))
+c = int(input("Nhap so nguyen thu ba: "))
+max_num= max(a, b, c)
+min_num= min(a, b, c)
+print("So lon nhat la: ", max_num)
+print("So nho nhat la: ", min_num)
