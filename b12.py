9a80552c98742335f1688de88abea31928f1f604
NguyenXuanAnhVy <anhvy12052005@gmail.com>
2024-08-29 18:10:00 +0700

    Add files via upload

---
+++ b12.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Aug 29 18:02:23 2024
+
+@author: Student
+"""
+
+import random
+a= random.randrange(0,100)
+print("Xuat ra mot so ngau nhien tu 0 den 100: ", a)
+b= random.randrange(50,99)
+print("Xuat ra mot so ngau nhien tu 50 den 99: ", b)
+c= random.randrange(-39,79)
+print("Xuat ra mot so ngau nhien tu -39 den 79: ", c)
+d= random.randrange(-79,-39)
+print("Xuat ra mot so ngau nhien tu -79 den -39: ", d)
