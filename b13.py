0dfdc7a3ac29ad570e8adbbb3c73cc1cc0631241
NguyenXuanAnhVy <anhvy12052005@gmail.com>
2024-08-29 18:46:43 +0700

    Add files via upload

---
+++ b13.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Aug 29 18:29:27 2024
+
+@author: Student
+"""
+
+a= input("Nhap ngay sinh: ")
+b= input("Nhap thang sinh: ")
+c= input("Nhap nam sinh: ")
+print("Ngay thang nam sinh:" + a + "/"+ b + "/"+ c )
+print("Ngay thang nam sinh:" + a + "/"+ b + "/"+ c [-2:])
+print("Ngay thang nam sinh:" + c + "/"+ b + "/"+ a )
+
